c490e8e27b02b01fdf03e5c77230775af7cce501
Abhinav Pandey <79640544+pixelcaliber@users.noreply.github.com>
2023-02-12 03:45:18 +0530

    sec fix 3

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL: "http://airgoworld.onrender.com/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
