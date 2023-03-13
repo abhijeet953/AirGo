a35d127937f7d56cbd71373018a84c093dc3ad97
abhijeet953 <chowdhuryabhijeet98@gmail.com>
2023-03-13 13:59:25 +0530

    random background

--- public/script/script.js
+++ public/script/script.js
@@ -20,3 +20,29 @@ function rotateHands() {
 rotateHands();
 setInterval(rotateHands, 1000);
 
+const images = [
+	'images/bg0.jpg',
+	'images/bg1.jpg',
+	'images/bg2.jpg',
+	'images/bg3.jpg',
+	'images/bg4.jpg',
+	'images/bg5.jpg',
+	'images/bg6.jpg',
+	'images/bg7.jpg',
+	'images/bg8.jpg',
+	'images/bg9.jpg',
+	'images/bg10.jpg',
+	'images/bg11.jpg',
+	'images/bg12.jpg',
+	'images/bg13.jpg',
+	'images/bg14.jpg',
+	'images/bg15.jpg',
+	'images/bg16.jpg',
+	'images/bg17.jpg'
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+document.body.style.backgroundImage = `url(${chosenImage})`;
+
+
+
